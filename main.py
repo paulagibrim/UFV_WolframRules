cbd99cae4cb5d2dcf178d28e3b834173ea083a3e
Paula Gibrim <pgibrim@gmail.com>
2023-05-06 17:04:00 -0300

    Formatação da main

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ import rules as rl
 import utils
 
 
-# Valores constantes para o trabalho
+# Valores contantes da aplicação (pode mudar conforme a visualização desejada)
 SIZE = 155
 STEPS = 60
 
-
+# Para apenas aplicar uma regra, utilize a função:
 # state = utils.apply_rule(rl.rule2, SIZE, STEPS, begin=0)
 
-
+# Para combinar duas regras, utilize a função:
 state = utils.compose_rules(rl.rule20, rl.rule31, SIZE, STEPS)
 
+# Para transformar o seu estado atual em imagem, utilize a função:
 image = utils.get_image(state, SIZE, STEPS)
 
+# Para exibir a imagem, utilize o método:
 image.show()
